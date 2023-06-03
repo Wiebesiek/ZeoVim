707aed3979d275a36129a22a00b9d00503e1fcbf
Zachariah Wiebesiek <zwiebesiek@dealeron.com>
2023-06-02 23:45:17 -0400

    Neotest added

---
+++ lua/zeovim/plugins/neotest.lua
@@ -0,0 +1,74 @@
+-- Source: https://www.lazyvim.org/plugins/extras/test.core
+return {
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"Issafalcon/neotest-dotnet",
+		},
+		lazy = true,
+		opts = {
+			adapters = {
+				["neotest-dotnet"] = {
+					-- Extra arguments for nvim-dap configuration
+					-- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
+					dap = { justMyCode = false },
+					-- Tell neotest-dotnet to use either solution (requires .sln file) or project (requires .csproj or .fsproj file) as project root
+					-- Note: If neovim is opened from the solution root, using the 'project' setting may sometimes find all nested projects, however,
+					--       to locate all test projects in the solution more reliably (if a .sln file is present) then 'solution' is better.
+					discovery_root = "solution"
+				}
+			},
+			status = { virtual_text = true },
+			output = { open_on_run = true },
+			quickfix = {
+				open = function()
+					if require("lazyvim.util").has("trouble.nvim") then
+						vim.cmd("Trouble quickfix")
+					else
+						vim.cmd("copen")
+					end
+				end,
+			},
+		},
+		config = function(_, opts)
+			local neotest_ns = vim.api.nvim_create_namespace("neotest")
+			vim.diagnostic.config({
+				virtual_text = {
+					format = function(diagnostic)
+						-- Replace newline and tab characters with space for more compact diagnostics
+						local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+						return message
+					end,
+				},
+			}, neotest_ns)
+
+			if opts.adapters then
+				local adapters = {}
+				for name, config in pairs(opts.adapters or {}) do
+					if type(name) == "number" then
+						if type(config) == "string" then
+							config = require(config)
+						end
+						adapters[#adapters + 1] = config
+					elseif config ~= false then
+						local adapter = require(name)
+						if type(config) == "table" and not vim.tbl_isempty(config) then
+							local meta = getmetatable(adapter)
+							if adapter.setup then
+								adapter.setup(config)
+							elseif meta and meta.__call then
+								adapter(config)
+							else
+								error("Adapter " .. name .. " does not support setup")
+							end
+						end
+						adapters[#adapters + 1] = adapter
+					end
+				end
+				opts.adapters = adapters
+			end
+
+			require("neotest").setup(opts)
+		end
+	}
+}
