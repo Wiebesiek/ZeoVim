91215a30d7c7931268fd188903c5a10a844fb36c
Zachariah Wiebesiek <zwiebesiek@dealeron.com>
2023-06-19 15:47:26 -0400

    Cleaned up telescope config.

--- lua/zeovim/core/mappings/telescope.lua
+++ lua/zeovim/core/mappings/telescope.lua
@@ -6,14 +6,16 @@ local set = vim.keymap.set
 ---------------
 -- TELESCOPE --
 ---------------
-set('n', '<leader>gb', function() builtin().git_branches() end, { desc = "Git branches"})
-set('n', '<leader>gc', function() builtin().git_commits() end, { desc = "Git commits"})
-set('n', '<leader>gt', function() builtin().git_status() end, { desc = "Git status"})
-set('n', '<leader>ff', function() builtin().find_files() end, { desc = "Find files"})
-set('n', '<leader>fg', function() builtin().live_grep() end, {desc = "Live grep"})
-set('n', '<leader>fb', function() builtin().buffers() end, { desc = "Buffers"})
-set('n', '<leader>fh', function() builtin().help_tags() end, { desc = "Help tags"})
-set('n', '<leader>fd', function() builtin().diagnostics() end, { desc = "Help tags"})
+set('n', '<leader>gb', function() builtin().git_branches() end, { desc = "Git branches" })
+set('n', '<leader>gc', function() builtin().git_commits() end, { desc = "Git commits" })
+set('n', '<leader>gt', function() builtin().git_status() end, { desc = "Git status" })
+set('n', '<leader>ff', function() builtin().find_files() end, { desc = "Find files" })
+set('n', '<leader>fg', function() builtin().live_grep() end, { desc = "Live grep" })
+set('n', '<leader>fb', function() builtin().buffers() end, { desc = "Buffers" })
+set('n', '<leader>fh', function() builtin().help_tags() end, { desc = "Help tags" })
+set('n', '<leader>fk', function() builtin().keymaps() end, { desc = "normal mode keymap" })
+set('n', '<leader>fd', function() builtin().diagnostics() end, { desc = "Diagnostics" })
+set('n', '<leader>ft', function() builtin().lsp_type_definitions() end, { desc = "Type Definition" })
 set('n', "<leader>fw",
 	function()
 		builtin().live_grep {
@@ -24,4 +26,4 @@ set('n', "<leader>fw",
 	end,
 	{ desc = "Live Grep --hidden --no-ignore" }
 )
-set('n', '<leader>f<CR>', function() builtin().resume() end, { desc = "Resume Last Picker"})
+set('n', '<leader>f<CR>', function() builtin().resume() end, { desc = "Resume Last Picker" })
--- lua/zeovim/plugins/telescope.lua
+++ lua/zeovim/plugins/telescope.lua
@@ -1,4 +1,4 @@
-return{
+return {
 	'nvim-telescope/telescope.nvim',
 	tag = '0.1.1',
 	dependencies = {
@@ -9,37 +9,35 @@ return{
 	opts = function()
 		local actions = require "telescope.actions"
 		return {
-		extensions = {
-			["ui-select"] = {
-				require("telescope.themes").get_dropdown {
-				},
-			}
-		},
-		defaults = {
-			-- No idea how this isn't the default
-			layout_config = {
-				horizontal = {
-					prompt_position = "top"
+			extensions = {
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown {
+					},
 				}
 			},
-			path_display = { truncate = true },
-			-- path_display = { smart = true, truncate = true },
-			sorting_strategy = "ascending",
-		},
-		pickers = {
-			buffers = {
-				mappings = {
-					i = {
-						["<c-x>"] = actions.delete_buffer + actions.move_to_top,
+			defaults = {
+				-- No idea how this isn't the default
+				layout_config = {
+					horizontal = {
+						prompt_position = "top"
+					}
+				},
+				path_display = { truncate = true },
+				sorting_strategy = "ascending",
+			},
+			pickers = {
+				buffers = {
+					mappings = {
+						i = {
+							["<c-x>"] = actions.delete_buffer + actions.move_to_top,
+						}
 					}
 				}
 			}
 		}
-	}
 	end,
 	config = function(_, opts)
 		require("telescope").setup(opts)
 		require("telescope").load_extension("ui-select")
 	end
 }
-
