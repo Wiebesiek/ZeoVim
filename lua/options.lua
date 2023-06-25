71a18dc8cee7e0eb7385e8675471565ed14fd69d
Zachariah Wiebesiek <zwiebesiek@dealeron.com>
2023-06-25 15:58:24 -0400

    Added fold options.
    These were originally for `ufo` but they seem fine for general use.

--- lua/options.lua
+++ lua/options.lua
@@ -10,6 +10,10 @@ vim.wo.scrolloff = 8
 vim.wo.wrap = false
 vim.opt.termguicolors = true
 
+vim.o.foldcolumn = '0' -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
 -- disabled for nvim-tree (see docs)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
