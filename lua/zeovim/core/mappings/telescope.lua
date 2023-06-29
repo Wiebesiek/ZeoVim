928b793aa67c3b27f6a9c4c18ae815ed0a39fbbb
Zachariah Wiebesiek <zwiebesiek@dealeron.com>
2023-06-29 14:22:29 -0400

    Added search all files mapping.

--- lua/zeovim/core/mappings/telescope.lua
+++ lua/zeovim/core/mappings/telescope.lua
@@ -10,6 +10,11 @@ set('n', '<leader>gb', function() builtin().git_branches() end, { desc = "Git br
 set('n', '<leader>gc', function() builtin().git_commits() end, { desc = "Git commits" })
 set('n', '<leader>gt', function() builtin().git_status() end, { desc = "Git status" })
 set('n', '<leader>ff', function() builtin().find_files() end, { desc = "Find files" })
+set('n', '<leader>fl',
+	function()
+		builtin().find_files{ hidden = true, no_ignore = true}
+	end,
+	{ desc = "Find files --hidden --no-ignore" })
 set('n', '<leader>fg', function() builtin().live_grep() end, { desc = "Live grep" })
 set('n', '<leader>fb', function() builtin().buffers() end, { desc = "Buffers" })
 set('n', '<leader>fh', function() builtin().help_tags() end, { desc = "Help tags" })
