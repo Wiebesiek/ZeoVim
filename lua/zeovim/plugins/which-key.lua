5d2df7ab349fe2101f642198c7b25c8dd58f10de
Zachariah Wiebesiek <zwiebesiek@dealeron.com>
2024-03-18 11:44:57 -0400

    added which-ke config

--- lua/zeovim/plugins/which-key.lua
+++ lua/zeovim/plugins/which-key.lua
@@ -1,10 +1,28 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
-  opts = {
-  }
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 300
+	end,
+	opts = {
+	},
+	config = function()
+		local wk = require("which-key")
+		wk.register({
+				g = {
+					name = "Telescope Git"
+				},
+				h = {
+					name = "GitSigns"
+				},
+				t = {
+					name = "Test Suite"
+				},
+			},
+			{
+				prefix = "<leader>",
+			}
+		)
+	end
 }
